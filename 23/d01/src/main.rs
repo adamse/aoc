13e2d19bec3de714a7d07b487ac7b26f0ebd363f
Adam Sandberg Ericsson <adam@sandbergericsson.se>
2023-12-01 13:35:42 +0000

    .

--- 23/d01/src/main.rs
+++ 23/d01/src/main.rs
@@ -1,55 +1,60 @@
-fn main() {
-    let c = std::fs::read_to_string("inp.txt").unwrap();
+#![feature(byte_slice_trim_ascii)]
+// 56465
+// 55902
+fn p1(c: &[u8]) -> u64 {
+    // assert!(c.is_ascii());
     let mut sum = 0u64;
-    for l in c.lines() {
-        sum += 10 * l.chars().find(char::is_ascii_digit).unwrap().to_digit(10).unwrap() as u64;
-        sum += l.chars().rev().find(char::is_ascii_digit).unwrap().to_digit(10).unwrap() as u64;
+    for line in c.split(|x| *x == b'\n') {
+        sum += 10 * (line.iter().copied().find(u8::is_ascii_digit).unwrap() - b'0') as u64;
+        sum += (line.iter().copied().rev().find(u8::is_ascii_digit).unwrap() - b'0') as u64;
     }
-    println!("{sum}");
+    sum
+}
+fn p2(c: &[u8]) -> u64 {
+    // let c = std::fs::read("inp2.txt").unwrap();
+    // let c = c.trim_ascii();
 
-    // let c = std::fs::read_to_string("inp2.txt").unwrap();
+    const NUMS: [&[u8]; 9] = [
+        b"one", b"two", b"three", b"four", b"five", b"six", b"seven", b"eight", b"nine",
+    ];
 
-    let vals = std::collections::HashMap::from([
-        ("one", 1),
-        ("two", 2),
-        ("three", 3),
-        ("four", 4),
-        ("five", 5),
-        ("six", 6),
-        ("seven", 7),
-        ("eight", 8),
-        ("nine", 9),
-        ("1", 1),
-        ("2", 2),
-        ("3", 3),
-        ("4", 4),
-        ("5", 5),
-        ("6", 6),
-        ("7", 7),
-        ("8", 8),
-        ("9", 9),
-    ]);
-    let matches = vals.keys().copied().collect::<Vec<_>>();
-
-    assert!(c.is_ascii());
 
     let mut sum = 0u64;
-    for line in c.lines() {
-        let mut tails = vec![];
-        for (ii, _) in line.char_indices() {
-            tails.push(&line[ii..]);
-        }
-        let a = tails.iter()
-            .find_map(|&tail|
-                matches.iter()
-                    .find_map(|&y| tail.starts_with(y).then_some(vals[y]))).unwrap();
-        let b = tails.iter().rev()
-            .find_map(|&tail|
-                matches.iter()
-                    .find_map(|&y| tail.starts_with(y).then_some(vals[y]))).unwrap();
+    for line in c.split(|x| *x == b'\n') {
+        let a = (0..line.len())
+            .find_map(|start| {
+                if line[start].is_ascii_digit() {
+                    Some((line[start] - b'0') as u64)
+                } else if let Some((_, val)) =
+                        NUMS.iter().zip(1u64..=9).find(|(n, _)| line[start..].starts_with(n)) {
+                    Some(val)
+                } else {
+                    None
+                }
+            }).unwrap();
+
+        let b = (0..line.len()).rev()
+            .find_map(|start| {
+                if line[start].is_ascii_digit() {
+                    Some((line[start] - b'0') as u64)
+                } else if let Some((_, val)) =
+                        NUMS.iter().zip(1u64..=9).find(|(n, _)| line[start..].starts_with(n)) {
+                    Some(val)
+                } else {
+                    None
+                }
+            }).unwrap();
+
         let v = 10 * a + b;
         // println!("{v}");
         sum += v;
     }
-    println!("{sum}");
+    sum
+}
+fn main() {
+    let c = std::fs::read("inp.txt").unwrap();
+    let c = c.trim_ascii();
+    println!("{}", p1(c));
+    println!("{}", p2(c));
+
 }
