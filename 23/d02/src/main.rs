ce43554c0b490cbfd4ff2090a0f66aa44bad53d0
Adam Sandberg Ericsson <adam@sandbergericsson.se>
2023-12-03 11:02:56 +0000

    2023-02 x

--- 23/d02/src/main.rs
+++ 23/d02/src/main.rs
@@ -1,79 +1,71 @@
 #[derive(Default, Debug, Clone)]
-struct Round {
+struct Game {
+    id: u32,
     red: u32,
     green: u32,
     blue: u32,
 }
 
-impl Round {
-    fn ok(&self, other: &Round) -> bool {
-        self.red >= other.red &&
-            self.green >= other.green &&
-            self.blue >= other.blue
-    }
-    fn max(&self, other: &Round) -> Round {
-        Round {
-            red: self.red.max(other.red),
-            green: self.green.max(other.green),
-            blue: self.blue.max(other.blue),
-        }
-    }
+impl Game {
     fn power(&self) -> u32 {
         self.red * self.green * self.blue
     }
 }
 
-fn parse(game: &str) -> (u32, Vec<Round>) {
+fn parse(game: &str) -> Game {
     let (game, rest) = game.split_once(": ").unwrap();
     let (_, game) = game.split_once(' ').unwrap();
-    let game = game.parse().unwrap();
+    let id = game.parse().unwrap();
 
-    let mut rounds = vec![];
-
+    let mut game = Game::default();
+    game.id = id;
     for reveals in rest.split("; ") {
-        let mut round = Round::default();
         for reveal in reveals.split(", ") {
             let (count, colour) = reveal.split_once(' ').unwrap();
             let count = count.parse().unwrap();
             match colour {
-                "red" => { round.red = count; },
-                "green" => { round.green = count; },
-                "blue" => { round.blue = count; },
+                "red" => { game.red = game.red.max(count); },
+                "green" => { game.green = game.green.max(count); },
+                "blue" => { game.blue = game.blue.max(count); },
                 _ => { panic!(); },
             }
         }
-        rounds.push(round);
     }
 
-    (game, rounds)
+    game
 }
 
-fn p1(games: &[(u32, Vec<Round>)]) -> u32 {
-    let max = Round {
-        red: 12,
-        green: 13,
-        blue: 14,
-    };
+fn p1(games: &[Game]) -> u32 {
+    let st = std::time::Instant::now();
 
-    games.iter()
-        .filter(|(i, rounds)| rounds.iter().all(|r| max.ok(r)))
-        .map(|x|x.0).sum()
+    let res = games.iter()
+        .filter(|g| g.red <= 12 && g.green <= 13 && g.blue <= 14)
+        .map(|x|x.id).sum();
+
+    eprintln!("p1: {}ns", st.elapsed().as_nanos());
+    res
 }
-fn p2(games: &[(u32, Vec<Round>)]) -> u32 {
-    games.iter().map(|(_, rounds)|
-            rounds.iter().cloned()
-                .reduce(|a, b| a.max(&b)).unwrap().power())
-        .sum()
+fn p2(games: &[Game]) -> u32 {
+    let st = std::time::Instant::now();
+    let res = games.iter().map(Game::power)
+        .sum();
+    eprintln!("p2: {}ns", st.elapsed().as_nanos());
+    res
 }
+//const INP: &[u8] = include_bytes!("../inp.txt");
 
 fn main() {
     //let inp = std::fs::read_to_string("inp1.txt").unwrap();
     let inp = std::fs::read_to_string("inp.txt").unwrap();
+    // let inp = unsafe {std::str::from_utf8_unchecked(INP)};
     let inp = inp.trim();
+    let st = std::time::Instant::now();
     let mut games = vec![];
     for line in inp.lines() {
         games.push(parse(line));
     }
+    eprintln!("parse: {}us", st.elapsed().as_micros());
     println!("{}", p1(&games));
     println!("{}", p2(&games));
+    eprintln!("total: {}us", st.elapsed().as_micros());
 }
